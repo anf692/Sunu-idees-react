9094718f9f0324b0a5f8812611383c959ceeeb03
anf692 <fassane348@gmail.com>
2026-06-11 10:16:38 +0000

    Ajout du dossier _utils pour les erreurs

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CardIdee from "../composants/CardIdee";
 import CardDetail from "../composants/CardDetails";
+import Erreur from "../_utils/Erreur";
 
 function Router() {
   return (
@@ -8,6 +9,8 @@ function Router() {
       <Routes>
         <Route path="/" element={<CardIdee />} />
         <Route path="/detail/:id" element={<CardDetail />} />
+
+        <Route path="*" element={<Erreur />} />
       </Routes>
     </BrowserRouter>
   );
